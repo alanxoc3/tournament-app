db29be09e6782f26bdcdef654d70b0ee07c6cb59
Johnathan Rudy <Johnathan Rudy>
2016-06-06 09:40:26 -0600

    added the onclick functions (still does nothing)

--- app/src/main/java/cs246/fencing_tournament/MainScreen.java
+++ app/src/main/java/cs246/fencing_tournament/MainScreen.java
@@ -2,8 +2,47 @@ package cs246.fencing_tournament;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
+
+    public Button viewContestant;
+    public Button bracketView;
+    public Button poolView;
+    public Button enterContestant;
+
+    public void init() {
+        viewContestant = (Button) findViewById(R.id.viewContestant);
+        bracketView =  (Button) findViewById(R.id.bracketView);
+        poolView =  (Button) findViewById(R.id.poolView);
+        enterContestant =  (Button) findViewById(R.id.enterContestant);
+
+        viewContestant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        bracketView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        poolView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        enterContestant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
