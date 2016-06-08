e664c394a84401486023b20f694b891dd630fe49
Johnathan Rudy <Johnathan Rudy>
2016-06-08 09:29:52 -0600

    added a second activity for EnterContestants, it does nothing

---
+++ app/src/main/java/cs246/fencing_tournament/EnterContestant.java
@@ -0,0 +1,13 @@
+package cs246.fencing_tournament;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterContestant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_contestant);
+    }
+}
--- app/src/main/java/cs246/fencing_tournament/MainScreen.java
+++ app/src/main/java/cs246/fencing_tournament/MainScreen.java
@@ -1,5 +1,6 @@
 package cs246.fencing_tournament;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -39,7 +40,9 @@ public class MainScreen extends AppCompatActivity {
         enterContestant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent action = new Intent(MainScreen.this,EnterContestant.class);
 
+                startActivity (action);
             }
         });
     }
