9fbc58d1c29ea4039fb6190d7fd420015f8481a3
Alan Morgan <alanxoc3@gmail.com>
2016-06-08 10:03:03 -0600

    Alan testing

---
+++ app/src/test/java/cs246/fencing_tournament/MatchDataTest.java
@@ -0,0 +1,17 @@
+package cs246.fencing_tournament;
+
+import android.test.InstrumentationTestCase;
+
+import org.junit.Test;
+
+/**
+ * Created by alanxoc3 on 6/8/16.
+ */
+@Test
+public class MatchDataTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        int bob = 3;
+        int reality = 4;
+        assertEquals(bob, 7);
+    }
+}
