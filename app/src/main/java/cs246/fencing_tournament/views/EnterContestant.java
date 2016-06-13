597922777a66cf936c185b86475265d8c95d47f0
Alan Morgan <alanxoc3@gmail.com>
2016-06-13 12:58:36 -0600

    Merged the branches.
    Hi people.

--- app/src/main/java/cs246/fencing_tournament/views/EnterContestant.java
+++ app/src/main/java/cs246/fencing_tournament/views/EnterContestant.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-<<<<<<< HEAD:app/src/main/java/cs246/fencing_tournament/views/EnterContestant.java
-import cs246.fencing_tournament.R;
-=======
 import java.util.ArrayList;
 import java.util.List;
->>>>>>> 54fe353fd6c9c1e6229d30b652e51526027e83fc:app/src/main/java/cs246/fencing_tournament/EnterContestant.java
+
+import cs246.fencing_tournament.R;
+import cs246.fencing_tournament.data.ContestantData;
+
 
 public class EnterContestant extends AppCompatActivity {
 
@@ -19,7 +19,7 @@ public class EnterContestant extends AppCompatActivity {
 
 
     public void done(View v){
-        List<ContestantData> contestants = new ArrayList<ContestantData> ();
+        List<ContestantData> contestants = new ArrayList<ContestantData>();
         Intent action = new Intent(EnterContestant.this, MainScreen.class);
         startActivity(action);
     }
