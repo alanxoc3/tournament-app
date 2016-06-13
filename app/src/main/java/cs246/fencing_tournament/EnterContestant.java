54fe353fd6c9c1e6229d30b652e51526027e83fc
Johnathan Rudy <Johnathan Rudy>
2016-06-13 10:00:41 -0600

    worked on Enter contestants more

--- app/src/main/java/cs246/fencing_tournament/EnterContestant.java
+++ app/src/main/java/cs246/fencing_tournament/EnterContestant.java
@@ -3,20 +3,27 @@ package cs246.fencing_tournament;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EnterContestant extends AppCompatActivity {
 
     public Button finished;
 
-    public void init(){
-        finished = (Button) findViewById(R.id.finished);
+
+    public void done(View v){
+        List<ContestantData> contestants = new ArrayList<ContestantData> ();
+        Intent action = new Intent(EnterContestant.this, MainScreen.class);
+        startActivity(action);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_contestant);
-        init();
+
     }
 }
