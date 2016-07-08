887244b17781ab2a3db3b7f7fec02c7e12cc1a0c
Austin Burke <austindburke573@gmail.com>
2016-07-08 10:25:04 -0600

    Commiting .idea so I can pull

--- app/src/main/java/cs246/fencing_tournament/screens/ViewContestants.java
+++ app/src/main/java/cs246/fencing_tournament/screens/ViewContestants.java
@@ -3,7 +3,18 @@ package cs246.fencing_tournament.screens;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cs246.fencing_tournament.data.ContestantData;
+import java.util.List;
+
 public class ViewContestants extends AppCompatActivity {
+
+    public List <ContestantData> contestants;
+
+    public void displayContestants() {
+        for (int i = 0; i < contestants.size(); ++i){
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
