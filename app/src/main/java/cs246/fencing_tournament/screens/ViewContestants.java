7eb9e1c0ec5092587f959fab05812d390a29d585
Austin Burke <austindburke573@gmail.com>
2016-07-13 10:40:58 -0600

    Updated ViewContestants and view_contestants.xml

--- app/src/main/java/cs246/fencing_tournament/screens/ViewContestants.java
+++ app/src/main/java/cs246/fencing_tournament/screens/ViewContestants.java
@@ -1,8 +1,9 @@
 package cs246.fencing_tournament.screens;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import cs246.fencing_tournament.R;
@@ -10,9 +11,9 @@ import cs246.fencing_tournament.data.ContestantData;
 
 import java.util.List;
 
-public class ViewContestants extends AppCompatActivity {
+public class ViewContestants extends ListActivity {
 
-    public List <ContestantData> contestants;
+    public List<ContestantData> contestants;
     private String[] myStringArray;
 
     @Override
@@ -23,11 +24,10 @@ public class ViewContestants extends AppCompatActivity {
 
         if (contestants.size() != 0) {
             myStringArray = new String[contestants.size()];
-            for (int i = 0; i < contestants.size(); ++i){
+            for (int i = 0; i < contestants.size(); ++i) {
                 myStringArray[i] = contestants.get(i).getName();
             }
-        }
-        else {
+        } else {
             myStringArray = new String[1];
             myStringArray[0] = "There are no Contestants";
         }
@@ -35,7 +35,19 @@ public class ViewContestants extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, myStringArray);
 
-        ListView listView = (ListView) findViewById(R.id.listview);
+        ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
     }
+
+    @Override
+    protected void onListItemClick(ListView listView, View v, int position, long id) {
+        super.onListItemClick(listView, v, position, id);
+        for (int i = 0; i < myStringArray.length; ++i) {
+            if (contestants.get(i).getName().contentEquals(myStringArray[position - 1])) {
+
+            }
+        }
+
+    }
 }
+
